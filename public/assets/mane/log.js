af6d678f26cb32c41251fe3aa61fcd15ea3b4a8b
Harvey <chenbolh@gmail.com>
2026-02-03 18:48:15 -0600

    first commit

--- public/assets/mane/log.js
+++ public/assets/mane/log.js
@@ -1,1 +1,39 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 6(1h){M k=o.1O(\'.1N-1M\');1L(1h){1g 1:k.c.C(\'A--1f\');k.c.D(\'A--1e\');1d;1g 0:k.c.D(\'A--1f\');k.c.C(\'A--1e\');1d}}$(o).I("F","#1K",2(e){e.t();H 5=$(8).j().j().J("#a").i("5");d(5=="a"){$(8).w("4-z 4-9");$(8).u("4-z 4-9-v");$(8).j().j().J("#a").i("5","b")}m d(5=="b"){$(8).w("4-z 4-9-v");$(8).u("4-z 4-9");$(8).j().j().J("#a").i("5","a")}});$(o).I("F","#1J",2(e){e.t();E y=$("#a");E x=$("#x");d(y.i("5")==="a"){y.i("5","b");x.w("4-9").u("4-9-v")}m{y.i("5","a");x.w("4-9-v").u("4-9")}});$("#1I").I(\'1b\',2(e){e.t();H $1a=$(8);6(1);1c.1H(2(){1c.1G(\'1F-1E\',{1D:\'1b\'}).1C(2(G){d(!N()){6(0);s.r({h:\'3\',g:\'¡q!\',b:"1B 1A 1z 1y 1x"});K}H 19=$1a.1w()+\'&G=\'+G;$.13({5:\'12\',11:\'10/Z/Y.X\',7:19,W:\'V\',U:2(){},T:2(7){17=("h"18 7)?7.h:\'3\';16=("g"18 7)?7.g:\'¡q!\';d(7[0]){15.14()}m{6(0);s.r({h:17,g:16,b:7[1]});1v(2(){15.14()},1u)}},3:2(f){6(0);s.r({h:\'3\',g:\'¡q!\',b:\'S p 3: \'+f.R+\', Qón p 3: \'+f.P})},O:2(1t){6(0)},})})})});$("#1s").F(2(e){e.t();$.13({5:\'12\',11:\'10/Z/Y.X\',7:{\'1r\':\'1q\'},W:\'V\',U:2(){6(1)},T:2(7){6(0)},3:2(f){6(0);s.r({h:\'3\',g:\'¡q!\',b:\'S p 3: \'+f.R+\', Qón p 3: \'+f.P})},O:2(){6(0)},})});2 N(){M L=o.1p(\'#1o [1n]\');E B=1m;L.1l(l=>{d(!l.1k.1j()){l.c.D(\'3\');B=1i}m{l.c.C(\'3\')}});K B}',62,113,'||function|error|fa|type|loaderefect|data|this|eye|password|text|classList|if||xhr|title|icon|attr|parent|LOADING|field|else||document|de|ERROR|fire|Swal|preventDefault|addClass|slash|removeClass|eyeIcon|passwordField|regular|loading|isValid|remove|add|let|click|token|var|on|find|return|fields|const|validateForm|complete|responseJSON|Informaci|status|Codigo|success|beforeSend|json|dataType|php|crud_usuario|cruds|src|url|POST|ajax|reload|location|titulo|icono|in|dataForm|form|submit|grecaptcha|break|show|hide|case|sh|false|trim|value|forEach|true|required|cuadro|querySelectorAll|salir|condi|eliminarsesion|dat|1500|setTimeout|serialize|obligatorios|campos|los|todos|Rellene|then|action|g0qAAAAAHmikE5cc8FL3ctmtjayshUNAbv8|6Ld1|execute|ready|frmlogin|togglePasswordindex|togglePassword|switch|container|loader|querySelector'.split('|'),0,{}))
+eval(
+  (function (p, a, c, k, e, d) {
+    e = function (c) {
+      return (
+        (c < a ? "" : e(parseInt(c / a))) +
+        ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+      );
+    };
+    if (!"".replace(/^/, String)) {
+      while (c--) {
+        d[e(c)] = k[c] || e(c);
+      }
+      k = [
+        function (e) {
+          return d[e];
+        },
+      ];
+      e = function () {
+        return "\\w+";
+      };
+      c = 1;
+    }
+    while (c--) {
+      if (k[c]) {
+        p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+      }
+    }
+    return p;
+  })(
+    '2 6(1h){M k=o.1O(\'.1N-1M\');1L(1h){1g 1:k.c.C(\'A--1f\');k.c.D(\'A--1e\');1d;1g 0:k.c.D(\'A--1f\');k.c.C(\'A--1e\');1d}}$(o).I("F","#1K",2(e){e.t();H 5=$(8).j().j().J("#a").i("5");d(5=="a"){$(8).w("4-z 4-9");$(8).u("4-z 4-9-v");$(8).j().j().J("#a").i("5","b")}m d(5=="b"){$(8).w("4-z 4-9-v");$(8).u("4-z 4-9");$(8).j().j().J("#a").i("5","a")}});$(o).I("F","#1J",2(e){e.t();E y=$("#a");E x=$("#x");d(y.i("5")==="a"){y.i("5","b");x.w("4-9").u("4-9-v")}m{y.i("5","a");x.w("4-9-v").u("4-9")}});$("#1I").I(\'1b\',2(e){e.t();H $1a=$(8);6(1);1c.1H(2(){1c.1G(\'1F-1E\',{1D:\'1b\'}).1C(2(G){d(!N()){6(0);s.r({h:\'3\',g:\'¡q!\',b:"1B 1A 1z 1y 1x"});K}H 19=$1a.1w()+\'&G=\'+G;$.13({5:\'12\',11:\'10/Z/Y.X\',7:19,W:\'V\',U:2(){},T:2(7){17=("h"18 7)?7.h:\'3\';16=("g"18 7)?7.g:\'¡q!\';d(7[0]){15.14()}m{6(0);s.r({h:17,g:16,b:7[1]});1v(2(){15.14()},1u)}},3:2(f){6(0);s.r({h:\'3\',g:\'¡q!\',b:\'S p 3: \'+f.R+\', Qón p 3: \'+f.P})},O:2(1t){6(0)},})})})});$("#1s").F(2(e){e.t();$.13({5:\'12\',11:\'10/Z/Y.X\',7:{\'1r\':\'1q\'},W:\'V\',U:2(){6(1)},T:2(7){6(0)},3:2(f){6(0);s.r({h:\'3\',g:\'¡q!\',b:\'S p 3: \'+f.R+\', Qón p 3: \'+f.P})},O:2(){6(0)},})});2 N(){M L=o.1p(\'#1o [1n]\');E B=1m;L.1l(l=>{d(!l.1k.1j()){l.c.D(\'3\');B=1i}m{l.c.C(\'3\')}});K B}',
+    62,
+    113,
+    "||function|error|fa|type|loaderefect|data|this|eye|password|text|classList|if||xhr|title|icon|attr|parent|LOADING|field|else||document|de|ERROR|fire|Swal|preventDefault|addClass|slash|removeClass|eyeIcon|passwordField|regular|loading|isValid|remove|add|let|click|token|var|on|find|return|fields|const|validateForm|complete|responseJSON|Informaci|status|Codigo|success|beforeSend|json|dataType|php|crud_usuario|cruds|src|url|POST|ajax|reload|location|titulo|icono|in|dataForm|form|submit|grecaptcha|break|show|hide|case|sh|false|trim|value|forEach|true|required|cuadro|querySelectorAll|salir|condi|eliminarsesion|dat|1500|setTimeout|serialize|obligatorios|campos|los|todos|Rellene|then|action|g0qAAAAAHmikE5cc8FL3ctmtjayshUNAbv8|6Ld1|execute|ready|frmlogin|togglePasswordindex|togglePassword|switch|container|loader|querySelector".split(
+      "|",
+    ),
+    0,
+    {},
+  ),
+);
